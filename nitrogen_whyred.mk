a734e3a81e2ac8792a88629a36d2884e4061008c
mocarafee <harif.rp05@gmail.com>
2020-07-07 12:50:51 +0900

    whyred: nos bringup

--- nitrogen_whyred.mk
+++ nitrogen_whyred.mk
@@ -28,11 +28,10 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-# Inhierit from AOSP
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit some common Nitrogen stuff.
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := nitrogen_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
